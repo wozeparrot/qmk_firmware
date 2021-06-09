239bfa4e08dbd5aabe94031a99071ea16c94c3e7
Woze Parrot <wozeparrot@gmail.com>
2021-06-09 08:12:52 -0400

    some keymap changes

--- keyboards/mushycloud/rules.mk
+++ keyboards/mushycloud/rules.mk
@@ -8,7 +8,7 @@ BOOTLOADER = caterina
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes       # Mouse keys
+MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -20,5 +20,6 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
+OLED_DRIVER_ENABLE = yes
 
 SPLIT_KEYBOARD = yes
